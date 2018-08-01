232e4905e26e135f36733ba8854fa00b30e87428
afrizaldm <afrizalmaehndra212@gmail.com>
2018-08-01 15:18:09 +0700

    set opacity

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
         <style>
             .navbar {
-                opacity: 0.9;
+                opacity: 0.8;
             }
         </style>
     </head>
