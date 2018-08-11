7e2914cac4b43cbb4fa1c11717738e5688344b54
afrizaldm <afrizalmahendra212@gmail.com>
2018-08-11 13:27:35 +0700

    edit Database

--- database/medilearn2.sql
+++ database/medilearn2.sql
@@ -1,5 +1,5 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
+-- Host:                         localhost
 -- Server version:               10.1.30-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win32
 -- HeidiSQL Version:             9.5.0.5196
@@ -12,7 +12,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping structure for table medilearn.chatbox
-DROP TABLE IF EXISTS `chatbox`;
 CREATE TABLE IF NOT EXISTS `chatbox` (
   `id_chatbox` int(11) NOT NULL AUTO_INCREMENT,
   `userid` varchar(20) DEFAULT NULL,
@@ -22,9 +21,8 @@ CREATE TABLE IF NOT EXISTS `chatbox` (
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table medilearn.chatbox: ~12 rows (approximately)
-DELETE FROM `chatbox`;
 /*!40000 ALTER TABLE `chatbox` DISABLE KEYS */;
-INSERT INTO `chatbox` (`id_chatbox`, `userid`, `chat`, `waktu`) VALUES
+INSERT IGNORE INTO `chatbox` (`id_chatbox`, `userid`, `chat`, `waktu`) VALUES
 	(1, '5130411107', 'Hello World!', '2018-04-15 22:46:45'),
 	(2, '14421028', 'Hi', '2018-04-15 23:16:41'),
 	(3, '3390', 'Nice to meet you', '2018-04-16 13:35:38'),
@@ -42,7 +40,6 @@ INSERT INTO `chatbox` (`id_chatbox`, `userid`, `chat`, `waktu`) VALUES
 /*!40000 ALTER TABLE `chatbox` ENABLE KEYS */;
 
 -- Dumping structure for table medilearn.jawaban
-DROP TABLE IF EXISTS `jawaban`;
 CREATE TABLE IF NOT EXISTS `jawaban` (
   `id_jawaban` int(11) NOT NULL AUTO_INCREMENT,
   `id_materi` int(11) DEFAULT NULL,
@@ -50,15 +47,13 @@ CREATE TABLE IF NOT EXISTS `jawaban` (
   `file_jawaban` varchar(255) DEFAULT NULL,
   `userid` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id_jawaban`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table medilearn.jawaban: ~3 rows (approximately)
-DELETE FROM `jawaban`;
+-- Dumping data for table medilearn.jawaban: ~0 rows (approximately)
 /*!40000 ALTER TABLE `jawaban` DISABLE KEYS */;
 /*!40000 ALTER TABLE `jawaban` ENABLE KEYS */;
 
 -- Dumping structure for table medilearn.media
-DROP TABLE IF EXISTS `media`;
 CREATE TABLE IF NOT EXISTS `media` (
   `id_materi` int(11) NOT NULL AUTO_INCREMENT,
   `userid` varchar(10) NOT NULL,
@@ -70,30 +65,28 @@ CREATE TABLE IF NOT EXISTS `media` (
   `file_video` varchar(255) DEFAULT NULL,
   `status` enum('ACTIVE','NOT ACTIVE') DEFAULT 'NOT ACTIVE',
   PRIMARY KEY (`id_materi`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Dumping data for table medilearn.media: ~1 rows (approximately)
-DELETE FROM `media`;
+-- Dumping data for table medilearn.media: ~0 rows (approximately)
 /*!40000 ALTER TABLE `media` DISABLE KEYS */;
+INSERT IGNORE INTO `media` (`id_materi`, `userid`, `tema`, `nama_materi`, `materi`, `file_materi`, `file_tugas`, `file_video`, `status`) VALUES
+	(2, '14421028', 'Fisika', 'testing', '<p>testing</p>', '6723-new-microsoft-word-document---copy.docx', '9147-new-microsoft-word-document.docx', '35260-10-second-video-fail-[360p].mp4', 'ACTIVE');
 /*!40000 ALTER TABLE `media` ENABLE KEYS */;
 
 -- Dumping structure for table medilearn.respon
-DROP TABLE IF EXISTS `respon`;
 CREATE TABLE IF NOT EXISTS `respon` (
   `id_respon` int(10) NOT NULL AUTO_INCREMENT,
   `userid` varchar(10) NOT NULL,
   `id_media` varchar(10) NOT NULL,
   `file_respon` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_respon`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table medilearn.respon: ~3 rows (approximately)
-DELETE FROM `respon`;
+-- Dumping data for table medilearn.respon: ~1 rows (approximately)
 /*!40000 ALTER TABLE `respon` DISABLE KEYS */;
 /*!40000 ALTER TABLE `respon` ENABLE KEYS */;
 
 -- Dumping structure for table medilearn.tugas
-DROP TABLE IF EXISTS `tugas`;
 CREATE TABLE IF NOT EXISTS `tugas` (
   `id_tugas` int(11) NOT NULL AUTO_INCREMENT,
   `nama_tugas` varchar(100) DEFAULT NULL,
@@ -105,12 +98,10 @@ CREATE TABLE IF NOT EXISTS `tugas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table medilearn.tugas: ~1 rows (approximately)
-DELETE FROM `tugas`;
 /*!40000 ALTER TABLE `tugas` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tugas` ENABLE KEYS */;
 
 -- Dumping structure for table medilearn.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `userid` varchar(20) NOT NULL,
@@ -125,9 +116,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table medilearn.users: ~12 rows (approximately)
-DELETE FROM `users`;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id_user`, `userid`, `first_name`, `last_name`, `alamat`, `email`, `username`, `password`, `level`) VALUES
+INSERT IGNORE INTO `users` (`id_user`, `userid`, `first_name`, `last_name`, `alamat`, `email`, `username`, `password`, `level`) VALUES
 	(2, '5130411097', 'Zed', 'Far', 'Jogja', 'zed@gmail.com', 'zed', '89e3eb66497b398d7d2250f0093903c6', 'teacher3'),
 	(3, '97808', 'Siswa', 'Satu', 'JJ UH 4 no.396', 'siswa@gmail.com', 'siswa', 'bcd724d15cde8c47650fda962968f102', 'student'),
 	(4, '14421028', 'Dhea', 'Lintang Suswandana', '-', 'guru1@gmail.com', 'dhea', '202cb962ac59075b964b07152d234b70', 'teacher'),
@@ -143,7 +133,6 @@ INSERT INTO `users` (`id_user`, `userid`, `first_name`, `last_name`, `alamat`, `
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Dumping structure for view medilearn.v_belajar
-DROP VIEW IF EXISTS `v_belajar`;
 -- Creating temporary table to overcome VIEW dependency errors
 CREATE TABLE `v_belajar` (
 	`id_materi` INT(11) NOT NULL,
@@ -158,7 +147,6 @@ CREATE TABLE `v_belajar` (
 ) ENGINE=MyISAM;
 
 -- Dumping structure for view medilearn.v_chat
-DROP VIEW IF EXISTS `v_chat`;
 -- Creating temporary table to overcome VIEW dependency errors
 CREATE TABLE `v_chat` (
 	`first_name` VARCHAR(50) NULL COLLATE 'latin1_swedish_ci',
@@ -168,7 +156,6 @@ CREATE TABLE `v_chat` (
 ) ENGINE=MyISAM;
 
 -- Dumping structure for view medilearn.v_jawab
-DROP VIEW IF EXISTS `v_jawab`;
 -- Creating temporary table to overcome VIEW dependency errors
 CREATE TABLE `v_jawab` (
 	`userid` VARCHAR(20) NULL COLLATE 'latin1_swedish_ci',
@@ -179,7 +166,6 @@ CREATE TABLE `v_jawab` (
 ) ENGINE=MyISAM;
 
 -- Dumping structure for view medilearn.v_kotakmasuk
-DROP VIEW IF EXISTS `v_kotakmasuk`;
 -- Creating temporary table to overcome VIEW dependency errors
 CREATE TABLE `v_kotakmasuk` (
 	`userid` VARCHAR(20) NULL COLLATE 'latin1_swedish_ci',
@@ -192,7 +178,6 @@ CREATE TABLE `v_kotakmasuk` (
 ) ENGINE=MyISAM;
 
 -- Dumping structure for view medilearn.v_tugas
-DROP VIEW IF EXISTS `v_tugas`;
 -- Creating temporary table to overcome VIEW dependency errors
 CREATE TABLE `v_tugas` (
 	`id_tugas` INT(11) NOT NULL,
@@ -204,31 +189,26 @@ CREATE TABLE `v_tugas` (
 ) ENGINE=MyISAM;
 
 -- Dumping structure for view medilearn.v_belajar
-DROP VIEW IF EXISTS `v_belajar`;
 -- Removing temporary table and create final VIEW structure
 DROP TABLE IF EXISTS `v_belajar`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_belajar` AS select `media`.`id_materi` AS `id_materi`,`users`.`first_name` AS `first_name`,`media`.`tema` AS `tema`,`media`.`nama_materi` AS `nama_materi`,`media`.`materi` AS `materi`,`media`.`file_materi` AS `file_materi`,`media`.`file_tugas` AS `file_tugas`,`media`.`file_video` AS `file_video`, media.`status` AS status from (`media` join `users`) where (`media`.`userid` = `users`.`userid`) ;
 
 -- Dumping structure for view medilearn.v_chat
-DROP VIEW IF EXISTS `v_chat`;
 -- Removing temporary table and create final VIEW structure
 DROP TABLE IF EXISTS `v_chat`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_chat` AS select `users`.`first_name` AS `first_name`,`chatbox`.`chat` AS `chat`,`chatbox`.`waktu` AS `waktu`,`users`.`level` AS `level` from (`chatbox` join `users`) where (`users`.`userid` = `chatbox`.`userid`) ;
 
 -- Dumping structure for view medilearn.v_jawab
-DROP VIEW IF EXISTS `v_jawab`;
 -- Removing temporary table and create final VIEW structure
 DROP TABLE IF EXISTS `v_jawab`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_jawab` AS select `jawaban`.`userid` AS `userid`,`users`.`first_name` AS `first_name`,`users`.`last_name` AS `last_name`,`jawaban`.`id_materi` AS `id_materi`,`jawaban`.`file_jawaban` AS `file_jawaban` from (`jawaban` join `users`) where (`users`.`userid` = `jawaban`.`userid`) ;
 
 -- Dumping structure for view medilearn.v_kotakmasuk
-DROP VIEW IF EXISTS `v_kotakmasuk`;
 -- Removing temporary table and create final VIEW structure
 DROP TABLE IF EXISTS `v_kotakmasuk`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_kotakmasuk` AS select `v_jawab`.`userid` AS `userid`,`v_jawab`.`first_name` AS `first_name`,`v_jawab`.`last_name` AS `last_name`,`v_belajar`.`tema` AS `tema`,`v_belajar`.`nama_materi` AS `nama_materi`,`v_belajar`.`first_name` AS `guru`,`v_jawab`.`file_jawaban` AS `file_jawaban` from (`v_jawab` join `v_belajar`) where (`v_belajar`.`id_materi` = `v_jawab`.`id_materi`) ;
 
 -- Dumping structure for view medilearn.v_tugas
-DROP VIEW IF EXISTS `v_tugas`;
 -- Removing temporary table and create final VIEW structure
 DROP TABLE IF EXISTS `v_tugas`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_tugas` AS select tugas.id_tugas AS id_tugas , `users`.`first_name` AS `first_name`,`tugas`.`nama_tugas` AS `nama_tugas`,`tugas`.`file_tugas` AS `file_tugas`, tugas.tema AS tema, tugas.`status` from (`tugas` join `users`) where (`users`.`userid` = `tugas`.`userid`) ;
